bc5ce03451d49ad87dee9a7aa6742b6ebff47e1b
henriquesd <hsd.foz@gmail.com>
2023-12-24 12:29:06 +0100

    Created project

---
+++ HttpFilesDemo/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using HttpFilesDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HttpFilesDemo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = Enumerable.Range(1, 3)
+                    .Select(i => new Product { Id = i, Name = $"Product {i}" })
+                    .ToList();
+
+            return Ok(products);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Product product)
+        {
+            return Ok(product);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int productId)
+        {
+            return Ok($"Product {productId} was deleted.");
+        }
+    }
+}
